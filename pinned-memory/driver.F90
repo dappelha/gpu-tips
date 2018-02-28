b50912d3549e07d670fb9c8239ef02917d40742f
David Appelhans <dappelh@us.ibm.com>
2018-02-27 20:10:17 -0500

    OpenACC test added

--- pinned-memory/driver.F90
+++ pinned-memory/driver.F90
@@ -4,15 +4,20 @@ module basicmovement
   use nvtx_mod
   use omp_lib
 
+  integer :: samples =20
+  integer, parameter :: gigabyte = 1024*1024*1024/8
+  integer :: N = 1*gigabyte
   character(len=30) :: str
-  CHARACTER(LEN=*), PARAMETER :: fmt = "(2X, A, T30, E8.3, T40, F8.3 )"
+  CHARACTER(LEN=*), PARAMETER :: fmt = "(2X, A, T30, G8.3, T40, F8.3 )"
   integer :: color
-  integer :: samples =1
+
 contains
 
 
+
+
   subroutine omp_HtoD(h_dummy,mem)
-    
+
     implicit none
 
     real(kind=8), allocatable, intent(in) :: h_dummy(:)
@@ -20,10 +25,15 @@ contains
 
     real(kind=8) :: t1, t2, T  
     integer :: ierr, i
-
-
-
-    ! omp way (pageable memory FAILS FOR 8 GB ARRAY):
+#if defined(__ibmxl__)
+    str = "omp device allocation"
+    call nvtxStartRange(str)
+    t1 = omp_get_wtime()
+    !$omp target enter data map(alloc:h_dummy)
+    ierr = cudaDeviceSynchronize()
+    t2 = omp_get_wtime()
+    call nvtxEndRange
+    write(*,fmt) str, t2-t1
   
     ! OpenMP memcopy
     str = "omp HtoD"
@@ -38,7 +48,52 @@ contains
     call nvtxEndRange
     write(*,fmt) str, t2-t1, samples*mem/(t2-t1)
 
+    !$omp target exit data map(delete:h_dummy)
+    ierr = cudaDeviceSynchronize()
+
+#endif
   end subroutine omp_HtoD
+
+
+  subroutine acc_HtoD(h_dummy,mem)
+
+    implicit none
+
+    real(kind=8), allocatable, intent(in) :: h_dummy(:)
+    real(kind=8), intent(in) :: mem
+
+    real(kind=8) :: t1, t2, T  
+    integer :: ierr, i
+
+#if defined(__PGI)
+
+    str = "acc device allocation"
+    call nvtxStartRange(str)
+    t1 = omp_get_wtime()
+    !$acc enter data create(h_dummy)
+    ierr = cudaDeviceSynchronize()
+    t2 = omp_get_wtime()
+    call nvtxEndRange
+    write(*,fmt) str, t2-t1
+
+  
+    ! OpenACC memcopy
+    str = "acc HtoD"
+    color = modulo(color+1,7)
+    call nvtxStartRange(str,color)
+    t1 = omp_get_wtime()
+    do i=1,samples
+       !$acc update self(h_dummy)
+       ierr = cudaDeviceSynchronize()
+    enddo
+    t2 = omp_get_wtime()
+    call nvtxEndRange
+    write(*,fmt) str, t2-t1, samples*mem/(t2-t1)
+
+    !$acc exit data delete(h_dummy)
+    ierr = cudaDeviceSynchronize()
+#endif
+  end subroutine acc_HtoD
 
 
   subroutine implicitCUDA_HtoD(d_dummy,h_dummy,mem)
@@ -54,7 +109,7 @@ contains
 
     ! Implicit CUDA way
     str = "Implicit CUDA HtoD"
-    color = 1!modulo(color+1,7)
+    color = modulo(color+1,7)
     call nvtxStartRange(str,color)
     t1 = omp_get_wtime()
     do i=1,samples
@@ -81,7 +136,7 @@ contains
 
     ! Explicit CUDA memcopy
     str = "Explicit CUDA HtoD"
-    color = 1!modulo(color+1,7)
+    color = modulo(color+1,7)
     call nvtxStartRange(str,color)
     t1 = omp_get_wtime()
     do i=1,samples
@@ -107,9 +162,6 @@ program main
 
   implicit none
 
-  integer :: threads, blocks
-
-  integer :: i, N
   real(kind=8), allocatable :: A(:)
   real(kind=8), pinned, allocatable :: p_A(:)
   real(kind=8), device, allocatable :: d_A(:)
@@ -118,18 +170,17 @@ program main
 
   integer :: ierr
 
-  !character(len=30) :: str
-  !CHARACTER(LEN=*), PARAMETER :: fmt = "(2X, A, T30, E8.3, T40, F8.3 )"
 
-  N = 1024*1024*1024/2
   
   !mem = real(8*N,kind=8)/1D9
   mem = 8*real(N,kind=8)/real(1024*1024*1024,kind=8)
 
 
   !write(*,fmt) "Array size: ", real(8*N*1D-9), " GB"
-  write(*,*) "Array size: ", mem , " GB"
-
+  print*, "****Pinned vs non-pinned memory transfer comparison***"
+  write(*,'(A,F8.4,A)') "Array size: ", mem , " GB "
+  write(*,'(A,I6)')  "Number of samples: ", samples
+  write(*,"(2X,A, T30, A, T40, A )") "Test", "sec", "Bandwidth GB/s"
 
   write(*,*) "Allocation Timings:"
 
@@ -161,180 +212,63 @@ program main
   write(*,fmt) str, t2-t1
 
 
-  str = "omp device allocation (A)"
-  call nvtxStartRange(str)
-  t1 = omp_get_wtime()
-  !$omp target data map(alloc:A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1
+  ! REGULAR PAGEABLE MEMORY:
+  write(*,*) "REGULAR PAGEABLE MEMORY:"
 
-  str = "omp device allocation (p_A)"
-  call nvtxStartRange(str)
-  t1 = omp_get_wtime()
-  !$omp target data map(alloc:p_A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1
+  ! Implicit CUDA way
+  call implicitCUDA_HtoD(d_A,A,mem)
   
+  ! Another CUDA way
+  call explicitCUDA_HtoD(d_A,A,mem)
+
+
+  ! USING PINNED MEMORY
+  write(*,*) "USING PINNED MEMORY"
+  
+  ! Implicit CUDA way
+  call implicitCUDA_HtoD(d_A,p_A,mem)
+
+  ! Another CUDA way
+  call explicitCUDA_HtoD(d_A,p_A,mem)
+
+
 
 
 
   ! REGULAR PAGEABLE MEMORY:
   write(*,*) "REGULAR PAGEABLE MEMORY:"
 
-  ! omp way (FAILS FOR 8 GB ARRAY):
-
-  str = "omp HtoD"
-  call nvtxStartRange(str,1)
-  t1 = omp_get_wtime()
-  !  !$omp target enter data map(to:A)
-  !  !$omp target exit data map(delete:A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
+  ! omp way
+  call omp_HtoD(A,mem)
 
   ! openacc way:
-  str = "openacc HtoD"
-  call nvtxStartRange(str,5)
-  t1 = omp_get_wtime()
-  !$acc enter data copyin(A)
-  !$acc exit data delete(A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
-
-
-
+  call acc_HtoD(A,mem)
+  
   ! Implicit CUDA way
   call implicitCUDA_HtoD(d_A,A,mem)
-
-  ! str = "Implicit CUDA HtoD"
-  ! call nvtxStartRange("Implicit CUDA HtoD",2)
-  ! t1 = omp_get_wtime()
-  ! d_A = A
-  ! ierr = cudaDeviceSynchronize()
-  ! t2 = omp_get_wtime()
-  ! call nvtxEndRange
-  ! write(*,fmt) str, t2-t1, mem/(t2-t1)
-
-
+  
   ! Another CUDA way
-  str = "Explicit CUDA HtoD"
-  call nvtxStartRange("Explicit CUDA HtoD",3)
-  t1 = omp_get_wtime()
-  ierr = cudaMemcpy(d_A,A,size(A))  !<------ this seems to not be executing
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange  
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
-
+  call explicitCUDA_HtoD(d_A,A,mem)
 
 
   ! USING PINNED MEMORY
   write(*,*) "USING PINNED MEMORY"
 
-
-  ! omp way (WORKS FOR 8 GB ARRAY):
-  str = "omp HtoD"
-  call nvtxStartRange("omp HtoD",1)
-  t1 = omp_get_wtime()
-!  !$omp target enter data map(to:p_A)
-!  !$omp target exit data map(delete:p_A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
+  ! omp way
+  call omp_HtoD(p_A,mem)
 
   ! openacc way:
-  str = "openacc HtoD"
-  call nvtxStartRange(str,5)
-  t1 = omp_get_wtime()
-  !$acc enter data copyin(p_A)
-  !$acc exit data delete(p_A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
-
+  call acc_HtoD(A,mem)
+  
   ! Implicit CUDA way
-
   call implicitCUDA_HtoD(d_A,p_A,mem)
 
-  ! str = "Implicit CUDA HtoD"
-  ! call nvtxStartRange("Implicit CUDA HtoD",2)
-  ! t1 = omp_get_wtime()
-  ! d_A = p_A
-  ! ierr = cudaDeviceSynchronize()
-  ! t2 = omp_get_wtime()
-  ! call nvtxEndRange
-  ! write(*,fmt) str, t2-t1, mem/(t2-t1)
-
   ! Another CUDA way
-  str = "Explicit CUDA HtoD"
-  call nvtxStartRange("Explicit CUDA HtoD",3)
-  t1 = omp_get_wtime()
-  ierr = cudaMemcpy(d_A,p_A,size(p_A))
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange  
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
+  call explicitCUDA_HtoD(d_A,p_A,mem)
 
 
-  ! REGULAR PAGEABLE MEMORY:
-  write(*,*) "REGULAR PAGEABLE MEMORY:"
-
-  ! omp way (FAILS FOR 8 GB ARRAY):
-
-  str = "omp HtoD"
-  call nvtxStartRange(str,1)
-  t1 = omp_get_wtime()
-!  !$omp target enter data map(to:A)
-!  !$omp target exit data map(delete:A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
-
-  ! openacc way:
-  str = "openacc HtoD"
-  call nvtxStartRange(str,5)
-  t1 = omp_get_wtime()
-  !$acc enter data copyin(A)
-  !$acc exit data delete(A)
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
-
-  ! Implicit CUDA way
-  str = "Implicit CUDA HtoD"
-  call nvtxStartRange("Implicit CUDA HtoD",2)
-  t1 = omp_get_wtime()
-  d_A = A
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
 
 
-  ! Another CUDA way
-  str = "Explicit CUDA HtoD"
-  call nvtxStartRange("Explicit CUDA HtoD",3)
-  t1 = omp_get_wtime()
-  ierr = cudaMemcpy(d_A,A,size(A))  !<------ this seems to not be executing
-  ierr = cudaDeviceSynchronize()
-  t2 = omp_get_wtime()
-  call nvtxEndRange  
-  write(*,fmt) str, t2-t1, mem/(t2-t1)
-
-
-  !$omp target exit data map(delete:A)
-  !$omp target exit data map(delete:p_A)
   print*, "completed"
 
 
