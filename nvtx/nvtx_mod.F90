ad8eb373fb490e4bdeecfb483b52ad7f3b0f3a1a
David Appelhans <dappelh@us.ibm.com>
2018-03-26 14:01:38 -0400

    improved nvtx for non-nested marking

--- nvtx/nvtx_mod.F90
+++ nvtx/nvtx_mod.F90
@@ -42,11 +42,37 @@ interface nvtxRangePop
   end subroutine
 end interface
 
+interface nvtxRangeStart
+   function nvtxRangeStartA(name) bind(C, name='nvtxRangeStartA')
+     use iso_c_binding
+     character(kind=C_CHAR) :: name(256)
+     !integer(C_INT64_T), value :: nvtxRangeStartA ! value is ignored anway?
+     integer(C_INT64_T) :: nvtxRangeStartA
+   end function nvtxRangeStartA
+
+   function nvtxRangeStartEx(event) bind(C, name='nvtxRangeStartEx')
+     use iso_c_binding
+     import:: nvtxEventAttributes
+     type(nvtxEventAttributes):: event
+     !integer(C_INT64_T), value :: nvtxRangeStartEx
+     integer(C_INT64_T) :: nvtxRangeStartEx
+   end function nvtxRangeStartEx
+end interface
+
+interface nvtxRangeEnd
+   subroutine nvtxRangeEnd(id) bind(C, name='nvtxRangeEnd')
+     use iso_c_binding
+     integer(C_INT64_T),value :: id ! it is important that this be by value.
+     ! c uses unsigned 64 bit int...should work as long as passed by value.
+   end subroutine nvtxRangeEnd
+end interface
+
 contains
 
-subroutine nvtxStartRange(name,id)
+subroutine nvtxStartRange(name,color,id)
   character(kind=c_char,len=*) :: name
-  integer, optional:: id
+  integer, optional:: color ! integer determining the color
+  integer(C_INT64_T), optional:: id ! returned unique id for the range
   type(nvtxEventAttributes):: event
   character(kind=c_char,len=256) :: trimmed_name
   integer :: i
@@ -59,17 +85,34 @@ subroutine nvtxStartRange(name,id)
   enddo
 
 
-  if ( .not. present(id)) then
-    call nvtxRangePush(tempName)
+  if ( .not. present(id) ) then
+     if ( .not. present(color)) then
+        call nvtxRangePush(tempName)
+     else
+        event%color=col(mod(color,7)+1)
+        event%message=c_loc(tempName)
+        call nvtxRangePushEx(event)
+     endif
   else
-    event%color=col(mod(id,7)+1)
-    event%message=c_loc(tempName)
-    call nvtxRangePushEx(event)
-  end if
-end subroutine
+     if ( .not. present(color)) then
+        id = nvtxRangeStartA(tempName)
+     else
+        event%color=col(mod(color,7)+1)
+        event%message=c_loc(tempName)
+        id = nvtxRangeStartEx(event)
+     endif
 
-subroutine nvtxEndRange
-  call nvtxRangePop
+  endif
+
+end subroutine nvtxStartRange
+
+subroutine nvtxEndRange(id)
+  integer(C_INT64_T), optional:: id !id corresponding with nvtxStartRange
+  if ( .not. present(id) ) then
+     call nvtxRangePop
+  else
+     call nvtxRangeEnd(id)
+  endif
 end subroutine
 
 end module nvtx_mod
