1580b49b2e37221d43fe6b5f30212b4b2c70130e
David Appelhans <dappelh@us.ibm.com>
2018-05-02 17:46:38 -0400

    added error checking and fixed arguement passing mistake

--- callingC/driver.F90
+++ callingC/driver.F90
@@ -18,7 +18,7 @@ program main
 
 
 
-  integer :: n
+  integer :: i,n
   integer, allocatable :: a(:,:), b(:,:)
   integer, allocatable, device :: d_a(:,:), d_b(:,:)
 
@@ -50,7 +50,7 @@ program main
      ! move data in the default stream (now non-blocking)
      istat = cudaMemCpy(d_a(1,q),a(1,q),n)
      istat = cudaMemCpy(d_b(1,q),b(1,q),n)
-     call calling_routine_c(n,d_a,d_b)
+     call calling_routine_c(n, d_a(:,q), d_b(:,q) )
      print*, cudaGetErrorString( cudaGetLastError() )
      ! move the data back. a = a+b
      istat = cudaMemCpy(a(1,q),d_a(1,q),n)     
@@ -59,6 +59,17 @@ program main
   istat = cudaDeviceSynchronize()
   istat = cudaforSetDefaultStream(old_stream) ! restore the original default stream
   
+  ! Check the data:
+  do q=1,nstreams
+     do i=1,n
+        if(a(i,q) .ne. 3) then
+           print *, "incorrect results at"
+           print *, "a(",i,",",q,") = ", a(i,q)
+           exit
+        endif
+     enddo
+  enddo
+
   deallocate(d_a)
   deallocate(a)
   deallocate(d_b)
