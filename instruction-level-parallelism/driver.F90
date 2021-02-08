8af3991f192ff89cf91088d76778f5eae32694a2
David Appelhans <dappelh@us.ibm.com>
2021-02-08 16:16:51 -0500

    pick up env value of ilp bounds

--- instruction-level-parallelism/driver.F90
+++ instruction-level-parallelism/driver.F90
@@ -48,7 +48,7 @@ contains
     case default
        call copyAtoB_nounroll(A,B,n,ilp)
     case(2)
-       call copyAtoB_unroll2(A,B,n)
+       call copyAtoB_unroll2_manual(A,B,n)
     case(3)
        call copyAtoB_unroll3(A,B,n)
     case(4)
@@ -116,6 +116,46 @@ contains
     call copyAtoB_guts(A,B,n,ilp)   
   end subroutine copyAtoB_unroll2
 
+  subroutine copyAtoB_unroll2_manual(A,B,n)
+    implicit none
+    integer, parameter :: ilp=2
+    integer :: n
+    real(kind=8),intent(inout) :: A(n,*), B(n,*)    
+    integer :: i,j, blocks, threads, tot_threads
+    real(kind=8) :: t1, t2, T, mem
+
+    tot_threads=80*1024 ! this is as many blocks*threads as we need to examine.
+
+    do threads = 32, 512, 512-32
+       do blocks = 0, tot_threads/threads, tot_threads/threads/160
+          if (blocks .eq. 0) cycle
+          t1 = omp_get_wtime()
+          !$acc parallel loop gang vector num_gangs(blocks) vector_length(threads)
+          do i = 1, n
+             do j = 1, ilp ! This loop will be unrolled if ilp is compile time constant.
+                B(i,j) = A(i,j)
+             enddo
+          enddo
+          !$acc end parallel
+          t2 = omp_get_wtime()
+          T = t2-t1
+
+          !mem = 8*real(n,kind=8)/real(1024*1024*1024,kind=8)
+          mem = 8*ilp*real(n,kind=8)/real(1000*1000*1000,kind=8)
+
+
+
+          !write(22,fmt) n*ilp, blocks, threads, blocks*threads, ilp, 2*mem/T
+          write(22,fmt) blocks, threads, blocks*threads, ilp, ilp*blocks*threads, 2*mem/T
+
+       enddo
+    enddo
+
+
+    call copyAtoB_guts(A,B,n,ilp)   
+  end subroutine copyAtoB_unroll2
+
+
   subroutine copyAtoB_unroll3(A,B,n)
     implicit none
     integer, parameter :: ilp=3
@@ -144,22 +184,30 @@ end module bandwidthtest
 
 
 program main
+
   use cudafor
   use nvtx_mod
   use bandwidthtest
-
+  implicit none
+  character(len=20) :: buffer
+  integer :: ilp, ilp_start, ilp_end
 
   open (unit = 22, file = "data.txt")
   !write(*,fmt) "Array size: ", real(8*N*1D-9), " GB"
   print*, "****Memory copy bandwidth***"
   write(22,"(2X,A, T15, A, T30, A, T45, A, T60, A, T75, A )") "blocks", "threads", "tot threads", "ilp", "ilp*tot_threads", "Bandwidth GB/s"
 
-  do ilp=1,4
+  call get_environment_variable("ilpstart", buffer)
+  read( buffer, '(I8)' )  ilp_start
+  call get_environment_variable("ilpend", buffer)
+  read( buffer, '(I8)' )  ilp_end
+
+  do ilp=ilp_start,ilp_end
      call copyAtoB(ilp)
   enddo
 
-  call copyAtoB(ilp=5)
-  call copyAtoB(ilp=8)
+  !call copyAtoB(ilp=5)
+  !call copyAtoB(ilp=8)
 
 
   close(22)
