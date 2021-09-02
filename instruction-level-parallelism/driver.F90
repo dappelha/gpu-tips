2f154823ac943be4df004df4dc89c6c3cfd84cb2
David Appelhans <dappelhans@nvidia.com>
2021-09-02 14:31:01 -0700

    used higher optimization -fast, gave better runtime parameter performance. Also cleaned up code significantly, more robust imputs

--- instruction-level-parallelism/driver.F90
+++ instruction-level-parallelism/driver.F90
@@ -33,23 +33,28 @@ contains
     real(kind=8) :: t1, t2, T, mem
 
     integer :: ierr, n, blocks, threads
-
+    logical, save :: firsttime=.true.
+    
     n = 100*80*1024/ilp
 
     allocate(A(n,ilp),B(n,ilp))
 
     !$acc enter data create(A,B)
 
-    !$acc kernels
-    A(:,:) = 1.0d+0
-    !$acc end kernels
-
+    if( firsttime==.true.) then
+       !$acc kernels
+       !A(:,:) = 1.0d+0
+       !$acc end kernels
+       firsttime = .false.
+    endif
 
     if(unroll .eq. .true.) then 
        select case (ilp)
 
        case default
           call copyAtoB_nounroll(A,B,n,ilp)
+       case(1)
+          call copyAtoB_unroll1(A,B,n)
        case(2)
           call copyAtoB_unroll2(A,B,n)
        case(3)
@@ -58,15 +63,28 @@ contains
           call copyAtoB_unroll4(A,B,n)
        case(5)
           call copyAtoB_unroll5(A,B,n)
+       case(6)
+          call copyAtoB_unroll6(A,B,n)
+       case(7)
+          call copyAtoB_unroll7(A,B,n)
        case(8)
           call copyAtoB_unroll8(A,B,n)
+       case(9)
+          call copyAtoB_unroll9(A,B,n)
+       case(10)
+          call copyAtoB_unroll10(A,B,n)
+       case(11)
+          call copyAtoB_unroll11(A,B,n)
+       case(12)
+          call copyAtoB_unroll12(A,B,n)
        end select
 
     else
        call copyAtoB_nounroll(A,B,n,ilp)
     endif
 
-
+    !$acc exit data delete(A,B)
+    
     deallocate(A,B)
 
   end subroutine copyAtoB
@@ -112,45 +130,84 @@ contains
 
   end subroutine copyAtoB_guts
 
+
   subroutine copyAtoB_nounroll(A,B,n,ilp)
     implicit none
     integer, intent(in) :: ilp
-    include "copyAtoB_guts.F90"
+    #include "copyAtoB_guts.F90"
   end subroutine copyAtoB_nounroll
 
-
+  subroutine copyAtoB_unroll1(A,B,n)
+    implicit none
+    integer, parameter :: ilp=1
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll1
+  
   subroutine copyAtoB_unroll2(A,B,n)
     implicit none
     integer, parameter :: ilp=2
-    include "copyAtoB_guts.F90"
-
+    #include "copyAtoB_guts.F90"
   end subroutine copyAtoB_unroll2
-
 
   subroutine copyAtoB_unroll3(A,B,n)
     implicit none
     integer, parameter :: ilp=3
-    include "copyAtoB_guts.F90"
+    #include "copyAtoB_guts.F90"
   end subroutine copyAtoB_unroll3
 
   subroutine copyAtoB_unroll4(A,B,n)
     implicit none
     integer, parameter :: ilp=4
-    include "copyAtoB_guts.F90"
+    #include "copyAtoB_guts.F90"
   end subroutine copyAtoB_unroll4
 
   subroutine copyAtoB_unroll5(A,B,n)
     implicit none
     integer, parameter :: ilp=5
-    include "copyAtoB_guts.F90"
+    #include "copyAtoB_guts.F90"
   end subroutine copyAtoB_unroll5
 
+  subroutine copyAtoB_unroll6(A,B,n)
+    implicit none
+    integer, parameter :: ilp=6
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll6
+
+  subroutine copyAtoB_unroll7(A,B,n)
+    implicit none
+    integer, parameter :: ilp=7
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll7
 
   subroutine copyAtoB_unroll8(A,B,n)
     implicit none
     integer, parameter :: ilp=8
-    include "copyAtoB_guts.F90"
+    #include "copyAtoB_guts.F90"
   end subroutine copyAtoB_unroll8
+
+  subroutine copyAtoB_unroll9(A,B,n)
+    implicit none
+    integer, parameter :: ilp=9
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll9
+
+  subroutine copyAtoB_unroll10(A,B,n)
+    implicit none
+    integer, parameter :: ilp=10
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll10
+
+  subroutine copyAtoB_unroll11(A,B,n)
+    implicit none
+    integer, parameter :: ilp=11
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll11
+
+  subroutine copyAtoB_unroll12(A,B,n)
+    implicit none
+    integer, parameter :: ilp=12
+    #include "copyAtoB_guts.F90"
+  end subroutine copyAtoB_unroll12
 
 end module bandwidthtest
 
@@ -162,28 +219,46 @@ program main
   use bandwidthtest
   implicit none
   character(len=20) :: buffer
-  integer :: ilp, ilp_start, ilp_end
+  integer :: ilp, ilp_start, ilp_end, istat
 
   !open (unit = 22, file = "data.txt")
   open (unit = 22, file = "/dev/stdout")
   !write(*,fmt) "Array size: ", real(8*N*1D-9), " GB"
   print*, "****Memory copy bandwidth***"
-  write(22,"(2X,A, T15, A, T30, A, T45, A, T60, A, T75, A )") "blocks", "threads", "tot threads", "ilp", "ilp*tot_threads", "Bandwidth GB/s"
 
-  call get_environment_variable("ilpstart", buffer)
-  read( buffer, '(I8)' )  ilp_start
-  call get_environment_variable("ilpend", buffer)
-  read( buffer, '(I8)' )  ilp_end
 
+
+  ! ilp start and stop are runtime
+  call get_environment_variable("ilpstart", buffer, status=istat)
+  if (istat .ge. 1) then
+     print*, "Setting ilpstart env variable failed,"
+     print*, "using a default ilpstart of 1."
+     ilp_start=1
+  else
+     read( buffer, '(I8)' )  ilp_start
+     print*, "ilp_start = ", ilp_start
+  endif
+     
+  call get_environment_variable("ilpend", buffer, status=istat)
+  if (istat .ge. 1) then
+     print*, "Setting ilpend env variable failed,"
+     print*, "using a default ilpend of 12."
+     ilp_end=12
+  else
+     read( buffer, '(I8)' )  ilp_end
+     print*, "ilp_end = ", ilp_end
+  endif
+
+  
   print*, "****with compile time parameter***"
-
+  write(22,"(2X,A, T15, A, T30, A, T45, A, T60, A, T75, A )") "blocks", "threads", "tot threads", "ilp", "ilp*tot_threads", "Bandwidth GB/s"
   ! measure bandwidth with unrolling
   do ilp=ilp_start,ilp_end
      call copyAtoB(ilp,unroll=.true.)
   enddo
 
-  print*, "****Without compile time parameter***"
-
+  print*, "****run time parameter***"
+  write(22,"(2X,A, T15, A, T30, A, T45, A, T60, A, T75, A )") "blocks", "threads", "tot threads", "ilp", "ilp*tot_threads", "Bandwidth GB/s"
   ! measure without unrolling
   do ilp=ilp_start,ilp_end
      call copyAtoB(ilp,unroll=.false.)
